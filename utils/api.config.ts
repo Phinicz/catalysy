e91ade1cda8ede23d9bd25671c3d8916aaae5642
qalbeali <qalbeali8@gmail.com>
2025-04-30 16:19:00 +0500

    vercel configuration completed

--- utils/api.config.ts
+++ utils/api.config.ts
@@ -1,5 +1,8 @@
 export const API_CONFIG = {
-  baseUrl: "http://localhost:3000/api/snag",
+  baseUrl:
+    process.env.NODE_ENV === "production"
+      ? "https://catalysy.vercel.app/api/snag"
+      : "http://localhost:3000/api/snag",
   headers: {
     "Content-Type": "application/json",
     "X-API-KEY": process.env.NEXT_PUBLIC_SNAG_API_KEY || "",
